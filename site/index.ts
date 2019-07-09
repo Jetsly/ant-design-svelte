6eef3d0e6df4f7ac923b6b603d8c191e31fd1cf2
Jetsly <cnljli@live.com>
2019-07-09 11:21:27 +0800

    fix: update site

--- site/index.ts
+++ site/index.ts
@@ -1,6 +1,6 @@
 import './theme/static/index.less';
 import 'highlight.js/styles/solarized-light.css';
-import App from './App';
+import App from './App.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
